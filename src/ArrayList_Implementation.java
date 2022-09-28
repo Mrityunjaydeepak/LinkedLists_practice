001722f8b3c802d5ef48c95e8fcefd3d90efdb50
mrityunjay <mrityunjaydeepak22@gmail.com>
2022-09-28 10:35:58 +0530

    implementation

---
+++ src/ArrayList_Implementation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ArrayList_Implementation {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(10);
+        arr.add(290);
+        System.out.println(arr.size());
+        System.out.println(arr.get(1));
+    }
+
+}
